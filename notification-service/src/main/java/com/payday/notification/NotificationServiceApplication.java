22a0652502160c36ec95e191f131807f4b291c0d
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-23 20:55:30 +0400

    Update NotificationServiceApplication.java

--- notification-service/src/main/java/com/payday/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/payday/notification/NotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.payday.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  *
