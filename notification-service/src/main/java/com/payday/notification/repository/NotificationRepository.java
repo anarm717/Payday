a216aa09fca66b44935cc506782a50c7c38e6940
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-23 20:56:19 +0400

    Update NotificationRepository.java

--- notification-service/src/main/java/com/payday/notification/repository/NotificationRepository.java
+++ notification-service/src/main/java/com/payday/notification/repository/NotificationRepository.java
@@ -5,6 +5,7 @@ import com.payday.notification.domain.Notification;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 /**
  * 
  * @author anar
