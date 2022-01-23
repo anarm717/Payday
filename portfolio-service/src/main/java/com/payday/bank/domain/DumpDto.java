efcfaae5273b9f7451ea8ba3ef85bb3e5da86208
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-23 15:26:02 +0400

    Update DumpDto.java

--- portfolio-service/src/main/java/com/payday/bank/domain/DumpDto.java
+++ portfolio-service/src/main/java/com/payday/bank/domain/DumpDto.java
@@ -3,14 +3,8 @@ package com.payday.bank.domain;
 import java.util.Date;
 
 public interface DumpDto {
-//    public Integer getAge();
-//    public String getName();
-//    public String getGenre();
-//    public String getEmail();
-//    public String getAddress();
-
-
-    String getUserName();
+  String getUserName();
+    BigDecimal getPrice();
     Integer getInitialQuantity();
     String getSymbol();
     Date getCompletionDate();
