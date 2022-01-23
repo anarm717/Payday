9227f74d380af2034ec9637141d33ef7dd4011bb
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-23 19:59:13 +0400

    Update Notification.java

--- portfolio-service/src/main/java/com/payday/bank/domain/Notification.java
+++ portfolio-service/src/main/java/com/payday/bank/domain/Notification.java
@@ -16,6 +16,7 @@ public class Notification {
 
 	private Integer id;
 	private String userName;
+	private String email;
 	private Date completionDate;
 	private Integer orderId;
 	private String message;
