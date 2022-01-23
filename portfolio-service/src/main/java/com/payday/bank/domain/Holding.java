9082a7369be5fb218f8086aea7dce9902e0d8e51
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-23 13:45:39 +0400

    Update Holding.java

--- portfolio-service/src/main/java/com/payday/bank/domain/Holding.java
+++ portfolio-service/src/main/java/com/payday/bank/domain/Holding.java
@@ -8,53 +8,16 @@ import java.math.BigDecimal;
  * @author anar
  *
  */
+@Data
 public class Holding {
 
 	private Integer id;
 	private String symbol;
 	private Integer quantity = 0;
+	private BigDecimal changePercent;
 	private BigDecimal purchaseValue = BigDecimal.ZERO;
 	private BigDecimal currentValue = BigDecimal.ZERO;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getSymbol() {
-		return symbol;
-	}
-
-	public void setSymbol(String symbol) {
-		this.symbol = symbol;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public BigDecimal getPurchaseValue() {
-		return purchaseValue;
-	}
-
-	public void setPurchaseValue(BigDecimal purchaseValue) {
-		this.purchaseValue = purchaseValue;
-	}
-
-	public BigDecimal getCurrentValue() {
-		return currentValue;
-	}
-
-	public void setCurrentValue(BigDecimal currentValue) {
-		this.currentValue = currentValue;
-	}
 
 
 	@Override
