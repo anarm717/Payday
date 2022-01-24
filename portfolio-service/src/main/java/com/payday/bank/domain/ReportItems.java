0dad55904fce98520631d2cf6b4023644f07fe81
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-24 12:18:12 +0400

    Update and rename DumpDto.java to ReportItems.java

--- portfolio-service/src/main/java/com/payday/bank/domain/ReportItems.java
+++ portfolio-service/src/main/java/com/payday/bank/domain/ReportItems.java
@@ -4,7 +4,7 @@ package com.payday.bank.domain;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public interface DumpDto {
+public interface ReportItems {
   String getUserName();
     BigDecimal getPrice();
     Integer getInitialQuantity();
