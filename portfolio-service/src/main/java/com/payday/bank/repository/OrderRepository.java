feafa839c981d26744942157ca3b8ff581b26d5c
Anar Mammadov <50947138+anarm717@users.noreply.github.com>
2022-01-24 12:09:02 +0400

    Update OrderRepository.java

--- portfolio-service/src/main/java/com/payday/bank/repository/OrderRepository.java
+++ portfolio-service/src/main/java/com/payday/bank/repository/OrderRepository.java
@@ -16,16 +16,8 @@ import java.util.List;
 public interface OrderRepository extends CrudRepository<Order,Integer> {
 
 	List<Order> findByUserName(String userName);
-	Order findByOrderId(Integer orderId);
-
-	/*"select * from (SELECT accountid, initialquantity,symbol,completiondate,'buying' as type FROM ORDERS "
-	"where completiondate>=sysdate-60 "
-	"union all "
-	"select accountid,quantity,symbol,completiondate,'selling' as type from sells "
-	"where completiondate>=sysdate-60) bb "
-	"order by bb.type,bb.completiondate asc "*/
-
-
+	Optional<Order>  findByOrderId(Integer orderId);
+	
 	@Query(value = "select * from (SELECT user_name as userName,price, initial_quantity as initialquantity,symbol,completiondate,'buying' as type" +
 			" FROM ORDERS where completiondate>=sysdate-60 and user_name = :userName " +
 			"union all " +
@@ -33,5 +25,4 @@ public interface OrderRepository extends CrudRepository<Order,Integer> {
 			"from sells where completiondate>=sysdate-60 and user_name = :userName ) bb  " +
 			"order by bb.type,bb.completiondate asc ",nativeQuery = true)
 	 List<DumpDto> report(@Param("userName") String userName);
-
 }
